ed8cbd71f544962413cd0e2edf8306bf26098a40
diegoucr29 <68042686+diegoucr29@users.noreply.github.com>
2021-06-12 20:55:00 -0600

    Create AnalisisDataset.py

---
+++ AnalisisDataset.py
@@ -0,0 +1,26 @@
+pip install folium
+
+%matplotlib inline
+%pylab inline
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import folium 
+from folium import plugins
+
+df= pd.read_csv("/content/drive/MyDrive/Colab Notebooks/ChicagoCrime_dataset_2001.csv", nrows=10000)
+
+
+df.rename(columns={'Case Number':'Case_Number',
+                        'Primary Type':'Primary_Type',
+                        'Location Description':'Location_Description',
+                        'X Coordinate':'X_Coordinate',
+                        'Y Coordinate':'Y_Coordinat',
+                        'Updated On':'Updated_On'},inplace=True)
+
+df.columns
+
+df.describe(include=np.object)
+
+df["Primary_Type"].unique()
